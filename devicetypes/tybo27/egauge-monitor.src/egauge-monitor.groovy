19142569c51c808078d62a6efd906d273eba079f
T&A <bb.basecamp@gmail.com>
2016-12-08 08:37:15 -0800

    Updated to improve comments, remove unused code, and update helper functions

--- devicetypes/tybo27/egauge-monitor.src/egauge-monitor.groovy
+++ devicetypes/tybo27/egauge-monitor.src/egauge-monitor.groovy
@@ -30,21 +30,21 @@ metadata {
     capability "Sensor"
     capability "Temperature Measurement"
         
-    attribute "refreshTime", "number"
+    // Net energy atttibutes
 	attribute "netEnergyToday", "number"
     attribute "gridEnergyToday", "number"
     attribute "solarEnergyToday", "number"
     
+    // Instantaneous Attributes
+    attribute "refreshTime", "number"
     attribute "gridPower", "number"
     attribute "solarPower", "number"
     attribute "frequency", "number"
-    
     attribute "voltage1", "number"
     attribute "voltage2", "number"
 }
 
 	simulator {
-		// TODO: define status and reply messages here
 	}
 
 	tiles(scale:2) {
@@ -57,7 +57,7 @@ metadata {
     	            ] )
         	}
             
-            // Refresh button
+            // Refresh button and time
             standardTile("refresh", "device.net", inactiveLabel: true, decoration: "flat", width: 2, height: 1) {
                 state "default", action:"refresh.refresh", icon:"st.secondary.refresh"
             }
@@ -84,7 +84,6 @@ metadata {
                     [value: 1, color: "#FF0000"],
     	            ])
         	}  
-            
             //Labels for Daily Energy
             valueTile("netTodayHeader", "device.temperature", inactiveLabel: true, width: 2, height: 1, decoration: "flat") {
    	         state("default", label: 'Net Daily kWh')//, icon: "st.Home.home1" )
@@ -96,7 +95,7 @@ metadata {
    	         state("default", label: 'Grid Daily kWh')//, icon: "st.Appliances.appliances17" )
         	}  
             
-            //Current Power
+            //Current Power Tiles
             standardTile("net", "device.power", width: 2, height: 2) {
    	         state("power", label: '${currentValue}', unit:"kW", icon: "st.Home.home1", backgroundColors: [
                     [value: 1, color: "#FF0000"],
@@ -115,7 +114,6 @@ metadata {
                     [value: 1, color: "#FF0000"],
     	            ])
         	}    
-            
             //Labels for current power
             valueTile("netCurrentHeader", "device.temperature", inactiveLabel: true, width: 2, height: 1, decoration: "flat") {
    	         state("default", label: 'Net Current kW')//, icon: "st.Home.home1" )
@@ -149,7 +147,6 @@ metadata {
                     [value: 130, color: "#FF0000"],
     	            ])
         	}  
-            
             //Labels for Frequency and Voltage
             valueTile("freqHeader", "device.temperature", inactiveLabel: true, width: 2, height: 1) {
    	         state("default", label: 'Frequency(Hz)')//, icon: "st.Home.home1" )
@@ -162,12 +159,17 @@ metadata {
         	}  
             
         main ("netEnergyMain")
-        details(["refresh", "refreshTime", "netEnergy", "solarEnergy", "gridEnergy", "netTodayHeader", "solarTodayHeader", "gridTodayHeader", "net", "solar", "grid","netCurrentHeader", "solarCurrentHeader", "gridCurrentHeader","frequency","voltage1","voltage2", "freqHeader", "v1Header","v2Header"])
+        details(["refresh", "refreshTime", 
+        	"netEnergy", "solarEnergy", "gridEnergy", "netTodayHeader", "solarTodayHeader", "gridTodayHeader", 
+            "net", "solar", "grid","netCurrentHeader", "solarCurrentHeader", "gridCurrentHeader",
+            "frequency","voltage1","voltage2", "freqHeader", "v1Header","v2Header"
+        ])
 	}
 }
 
-
-// parse events into attributes
+/* ******************************************************************************************
+* Parse hubAction returned queries into power and energy data							 	*
+*********************************************************************************************/
 def parse(String description) {
 	//log.debug "Parsing '${description}'"
 	def msg = parseLanMessage(description)
@@ -177,20 +179,16 @@ def parse(String description) {
     def status = msg.status          // => http status code of the response
 	def roundResolution = settings.resolution // handle undefined case
     
-	//log.debug "headersAsString: $headersAsString"
-    //log.debug "headerMap: $headerMap"
-    //log.debug "body: $body"
     log.debug "HubAction Returned status: $status"
 
 	if (status>=200 && status<300) {
 		body = removeBodyHeaders(body)
-		//log.debug "updated body: $body"
 		
-		//getXML
+		// Parse XML for data
 		def rootNode =  new XmlSlurper().parseText(body)
-		//log.debug "XML root node: ${rootNode.name()}"
 		switch (rootNode.name()){
-			case 'group':
+			// rootNode of group indicates return from getDailyValuesHub
+            case 'group':
                 def groupData = [:]	
 				groupData.put("timeStamp", [:])
                 def numColumns = rootNode.data[0].@columns
@@ -203,14 +201,13 @@ def parse(String description) {
                 def solarToday
                 def temp
                 
+                // Loop through data fileds and capture name, time and value
                 rootNode.data.eachWithIndex { data, dIdx ->
 					
                     String timeStamp = data.@time_stamp
-                    long timeUTC = Long.parseLong(timeStamp[2..-1],16)
+                    def timeUTC = convertHexToLong (timeStamp)
                     String timeDeltaText = data.@time_delta
                     int timeDelta = timeDeltaText.toInteger()
-                    
-                    //log.debug "Data for dIdx:$dIdx contains timeStamp=$timeStamp($timeUTC), timeDelta=$timeDelta"
 					
                     if (dIdx==0) {
                         data.cname.eachWithIndex { cname, idx ->
@@ -223,12 +220,10 @@ def parse(String description) {
                                 log.debug "Did not add cname type=${cname.@t}, cname text=cname"
                             }
                         }
-                        //log.debug "groupData map updated with $groupData"
                     }
                     data.r.eachWithIndex { r, rIdx ->
                         r.c.eachWithIndex { c, cIdx ->
                         	def timeValue = timeUTC - timeDelta*rIdx
-                        	//log.debug "Value loop at rIdx=$rIdx, cIdx=$cIdx and cumIdx=$cumIdx, timeValue=$timeValue(orig=$timeUTC)"
                             groupData["timeStamp"].put(cumIdx, timeUTC - timeDelta*rIdx)
                             groupData[cnameMap[cIdx]].put(cumIdx, c.toFloat()/3600000)
                             
@@ -240,12 +235,14 @@ def parse(String description) {
                 groupData.each {key, value ->
 					log.debug "$key contains $value"
 				}
+                
+                // Only create/fire event if exactly three data points are returned (only three times requested)
                 if (groupData["timeStamp"].size()==3) {
                     def midnightUTC = (timeToday("00:00", location.timeZone).time/1000)//.round(0)
                     if (midnightUTC >= groupData["timeStamp"][0]) {
                         midnightUTC = midnightUTC - 24*60
                     }
-
+					// Priginal data requested in descending order, first is now, figure out which of the next two is midnight and which is x minutes ago
                     if ((groupData["timeStamp"][1]-midnightUTC).abs() < (groupData["timeStamp"].get(2)-midnightUTC).abs()) {
                         solarToday = groupData["SolarPlus"][0]-groupData["SolarPlus"][1]
                         gridToday = groupData["Grid"][0]-groupData["Grid"][1]
@@ -257,6 +254,7 @@ def parse(String description) {
                         netToday =gridToday-solarToday
                         temp = groupData["Grid"][0]-groupData["Grid"][1]
                     }
+                    // Create and return events
                     log.debug "solarToday=${solarToday.round(3)}kWh, gridToday=${gridToday.round(3)}kWh, netToday=${netToday.round(3)}kWh, temp=${temp.round(3)}kWh"
                     def gridEnergyEvent = createEvent(name: 'gridEnergyToday', value: gridToday.round(roundResolution))
                     def solarEnergyEvent = createEvent(name: 'solarEnergyToday', value: solarToday.round(roundResolution))
@@ -267,7 +265,7 @@ def parse(String description) {
                 	log.debug "Invalid parse, groupData size=${groupData["timeStamp"].size()}"
                 }
 				break
-                
+            // rootNode of measurements indicates return from getInstantValuesHub
 			case 'measurements':
                 def timeStamp = (rootNode.timestamp).toLong()
                 def refreshDate = new Date(timeStamp*1000)
@@ -285,40 +283,40 @@ def parse(String description) {
                 def toDateSolarEnergy = 0
                 def toDateGridEnergy = 0
                 
+                // Parse Meters for Grid and Solar 
                 rootNode.meter.each { meter ->
                     log.debug meter.name() + ":"+ meter.@title + ":" + meter.text()
-                    if (meter.name().equals("meter") && meter.@title.equals("Grid")) {
+                    if (meter.@title.equals("Grid")) {
                         currentGridPower = (meter.power.toFloat()/1000).round(roundResolution)
                         toDateGridEnergy = (meter.energy.toFloat())
                     }
-                    if (meter.name().equals("meter") && meter.@title.equals("Solar")) {
+                    if (meter.@title.equals("Solar")) {
                         currentSolarPower = (meter.power.toFloat()/1000).round(roundResolution)
                         toDateSolarEnergy = (meter.energy.toFloat())
                     }
-
                 }
-                log.debug "currentGridPower=${currentGridPower}"
-                log.debug "currentSolarPower=${currentSolarPower}"
 
                 currentNetPower = (currentGridPower - currentSolarPower).round(roundResolution)
                 toDateNetEnergy = toDateGridEnergy - toDateSolarEnergy
-                log.debug "currentNetPower=${currentNetPower}"
+                log.debug "currentGridPower=${currentGridPower} and currentSolarPower=${currentSolarPower}, currentNetPower=${currentNetPower}"
 
                 currentFreq = rootNode.frequency.toFloat().round(1)
                 log.debug "currentFrequency=$currentFreq"
 
+				// Loop through voltages to captrue both channels
                 rootNode.voltage.each { v ->
                     log.debug v.name() + ":"+ v.@ch + ":" + v.text()
                     if (v.name().equals("voltage") && v.@ch.equals("0")) {
-                        log.debug "Found Ch1 Voltage"
                         currentV1 = v.toFloat().round(1)
+                        log.debug "Ch1 Voltage=$currentV1"
                     }
 
                     if (v.name().equals("voltage") && v.@ch.equals("1")) {
-                        log.debug "Found Ch2 Voltage"
                         currentV2 = v.toFloat().round(1)
+                        log.debug "Ch1 Voltage=$currentV2"
                     }
                 }   
+                // Create and return events
                 def powerEvent = createEvent(name: 'power', value: (currentNetPower))
                 def gridPowerEvent = createEvent(name: 'gridPower', value: (currentGridPower))
                 def solarPowerEvent = createEvent(name: 'solarPower', value: (currentSolarPower))
@@ -328,36 +326,48 @@ def parse(String description) {
                 def refreshTimeEvent = createEvent(name: 'refreshTime', value: (refreshTime))
                 return [powerEvent, gridPowerEvent, solarPowerEvent, frequencyEvent, v1Event, v2Event, refreshTimeEvent]
 				break
+                
 			default:
-                log.debug "default"
+                log.debug "default reached on rootNode.name switch statement"
 				break
 		}
 	}
 }
 
+/* ******************************************************************************************
+* Poll is run automatically every several minutes, only request dailyValues since those  	*
+* are displayed as main, and used as temperature in Hub Dashboard							*
+*********************************************************************************************/
 def poll() {
 	log.debug "Executing 'poll'"
     getDailyValuesHub()
 }
 
+/********************************************************************************************
+* Refresh is requested when on the device page, update both daily and instantaneous numbers	*
+********************************************************************************************/
 def refresh() {
   log.debug "Executing 'refresh'"
   getDailyValuesHub()
   getInstantValuesHub()
 }
 
+/********************************************************************************************
+* getDailyValuesHub calculates current time, x mintues ago, and midnight today to send a	*
+* hubAction to the egauge requesting energy data for those times							*
+********************************************************************************************/
 def getDailyValuesHub () {
-	def offset=10 // offset for api call in s
+	log.debug "Executing 'getDailyValuesHub'"
+    def offset=10 // offset for api call in s (ensure data is written to register already)
     def nowUTC = Math.round( now()/1000 - offset )  //.round(0)
     def midnightUTC = (timeToday("00:00", location.timeZone).time/1000)//.round(0)
     def windowUTC = nowUTC - settings.usageWindowThresh*60
     
     def times = [nowUTC, windowUTC, midnightUTC]
-    times = times.sort().reverse()
-	//def T = "${times[0]},${times[1]},${times[2]}"
+    times = times.sort().reverse() // times must be sent in descending order
     log.debug "T=${times[0]},${times[1]},${times[2]}"
-    
-	log.debug "Executing 'getDailyValuesHub'"
+	
+    // Define and return hubAction
 	def result = new physicalgraph.device.HubAction(
         method: "GET",
         path: "/cgi-bin/egauge-show",
@@ -365,13 +375,14 @@ def getDailyValuesHub () {
             HOST: getHostAddress()
         ],
         query: [T: "${times[0]},${times[1]},${times[2]}"]
-		)
-    //log.debug "result= $result"
-    
+	)
     result
     return result
 }
 
+/********************************************************************************************
+* getInstantValuesHub sends a hubAction to the egauge requesting 'noteam' power data		*
+********************************************************************************************/
 def getInstantValuesHub () {
 	
 	log.debug "Executing 'getInstantValuesHub'"
@@ -381,16 +392,18 @@ def getInstantValuesHub () {
         headers: [
             HOST: getHostAddress()
         ],
-        //query: ["noteam"]
-		)
-    //log.debug "result= $result"
-    
+	)
     result
     return result
 }
 
+
+/********************************************************************************************
+* removeBodyHeaders removes first line (xml tag) and second if !DOCTYPE to clean up for 	*
+* XMLslurper																				*
+********************************************************************************************/
 def removeBodyHeaders (body) {
-	// Removes first line and second if <!DOCTYPE 
+	
 	def bodyList = body.readLines()
 	body = bodyList[1..bodyList.size-1].join("")
     
@@ -402,10 +415,16 @@ def removeBodyHeaders (body) {
 	return body
 }
 
+/********************************************************************************************
+* convertIPToHex converts in ipadress to hex (currently unused)								*																				*
+********************************************************************************************/
 private String convertIPToHex(ipAddress) {
 	return Long.toHexString(converIntToLong(ipAddress));
 }
 
+/********************************************************************************************
+* converIntToLong converts in ipadress:port from int to long (currently unused)				*																				*
+********************************************************************************************/
 private Long converIntToLong(ipAddress) {
 	log.debug "from Long converIntToLong = $ipAddress"
 	long result = 0
@@ -416,6 +435,10 @@ private Long converIntToLong(ipAddress) {
     return result & 0xFFFFFFFF;
 }
 
+/********************************************************************************************
+* getHostAddress grabs hex device network ID (IpAddress:port in HEX!!!)and parses to 		*
+* decimal format (nnn.nnn.nnn.nnn:xxxx) 													*																				*
+********************************************************************************************/
 private getHostAddress() {
 	def parts = device.deviceNetworkId.split(":")
 	def ip = convertHexToIP(parts[0])
@@ -423,9 +446,26 @@ private getHostAddress() {
 	return ip + ":" + port
 }
 
+/********************************************************************************************
+* convertHexToInt converts hex value to integer												*																				*
+********************************************************************************************/
 private Integer convertHexToInt(hex) {
 	Integer.parseInt(hex,16)
 }
+
+/********************************************************************************************
+* convertHexToInt converts hex value to long												*																				*
+********************************************************************************************/
+private Long convertHexToLong(hex) {
+    if (hex[0..1]=="0x") {
+    	hex = hex[2..-1]
+    }
+    Long.parseLong(hex,16)
+}
+
+/********************************************************************************************
+* convertHexToIP converts hex ip value to string in nnn.nnn.nnn.nnn format												*																				*
+********************************************************************************************/
 private String convertHexToIP(hex) {
 	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
 }
